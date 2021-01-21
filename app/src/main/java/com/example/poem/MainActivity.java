0b2b1ec7d2e0d1fd3249269eafa662cc8ebb6684
DESKTOP-LESNCAF <shahmirkhan039@gmail.com>
2021-01-21 10:58:28 +0500

    Initial commit

---
+++ app/src/main/java/com/example/poem/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.poem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.poem.playpoem.playfirst;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CardView cardView = (CardView)findViewById(R.id.card2);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+
+    public void card1(View view) {
+        Toast.makeText(this, "poem1", Toast.LENGTH_SHORT).show();
+        Intent intent=new Intent(MainActivity.this, playfirst.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/poem/playpoem/playfirst.java
@@ -0,0 +1,79 @@
+package com.example.poem.playpoem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.example.poem.MainActivity;
+import com.example.poem.R;
+
+public class playfirst extends AppCompatActivity {
+    VideoView videoView;
+    Uri uri;
+    MediaPlayer md;
+    ImageView back;
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playfirst);
+        b1 =(Button)findViewById(R.id.b1);
+        back= (ImageView)findViewById(R.id.back);
+        videoView = (VideoView) findViewById(R.id.video);
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Toast.makeText(playfirst.this, "video is completed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        MediaController mediaController = new MediaController(playfirst.this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            playFunction();
+            b1.setClickable(false);
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // call function
+                backfunction();
+            }
+        });
+
+
+
+
+    }
+    public void backfunction()
+    {
+        // intent code
+        Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(myIntent);
+    }
+    public void playFunction()
+    {
+        uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.poem);
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+
+    }
+}
